c64d53dfceb68c84b1aa61c6d4b0beca31a16028
Nurbek Amanbek <nurbek_aman@mail.ru>
2022-04-29 11:31:08 +0600

    Eslint

--- cart.js
+++ cart.js
@@ -1,44 +1,44 @@
 'use strict';
 const cart = {
 
-        items: [],
-        totalPrice: 0,
-        count: 0,
-        
-add(productName, productPrice, productCount = 1){
+  items: [],
+  totalPrice: 0,
+  count: 0,
+
+  add(productName, productPrice, productCount = 1) {
     const items = {
-    'productName': productName,
-    'productPrice': productPrice,
-    'productCount': productCount
+      productName,
+      productPrice,
+      productCount,
     };
     this.items.push(items);
     this.increaseCount(productCount);
-},
+  },
 
- increaseCount(productCount) {
+  increaseCount(productCount) {
     this.count += productCount;
-},
+  },
 
- calculateItemPrice() {
-    return  this.items.reduce((total, item) => total + (item.productPrice * item.productCount), 0)
-},
+  calculateItemPrice() {
+    return this.items.reduce((total, item) => total + (item.productPrice * item.productCount), 0);
+  },
 
- clear() {
-     this.items = [];
-     this.totalPrice = 0;
-     this.count = 0;
-},
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  },
 
- print() {
+  print() {
     console.log(`${JSON.stringify(this.items)}\n Total: ${this.totalPrice}`);
-},
+  },
 };
 
-Object.defineProperty(cart, 'totalPrice' ,{
-    get() {
-      return this.calculateItemPrice();
-    },
-    });
+Object.defineProperty(cart, 'totalPrice', {
+  get() {
+    return this.calculateItemPrice();
+  },
+});
 
 cart.add('Apple', 150, 3);
 cart.add('Bread', 50, 1);
